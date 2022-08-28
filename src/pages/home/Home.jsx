625f34203af514c6a322ed4cc96917823f67972a
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-08-28 22:07:38 +0530

    Added and Styled the Table Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 
 const Home = () => {
     return (
@@ -14,7 +15,7 @@ const Home = () => {
                 <Navbar />
                 <div className="widgets">
                     <Widget type="user" />
-                    <Widget type="order" />
+                <Widget type="order" />
                     <Widget type="earning" />
                     <Widget type="balance" />
                 </div>
@@ -22,6 +23,11 @@ const Home = () => {
                 <div className="charts">
                     <Featured />
                     <Chart />
+                </div>
+
+                <div className="listContainer">
+                    <div className="listTitle">Latest Transactions</div>
+                    <Table />
                 </div>
             </div>
         </div>
